9df01b5069880c6f52035a90e723b4b362dfdc37
Christian Staib <staib.christian@hotmail.de>
2024-06-20 22:18:54 +0200

    did stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,73 +1,27 @@
-#include "graph.hpp"
-
-#include <chrono>
-#include <cstdint>
-#include <cstdio>
-#include <fstream>
-#include <iostream>
 #include <mpi.h>
-#include <nlohmann/json.hpp>
+#include <omp.h>
+#include <stdio.h>
 
-using json = nlohmann::json;
+int main(int argc, char *argv[]) {
+  int my_id, omp_rank;
+  int provided, required = MPI_THREAD_FUNNELED;
 
-int main(int argc, char **argv) {
-  //  int rc;
-  //  int mpi_rank_global, mpi_numPEs_global, procnamelen;
-  //  int mpiversion, mpisubversion;
-  //  char processor_name[MPI_MAX_PROCESSOR_NAME];
-  //  MPI_Comm mpi_comm_global = MPI_COMM_WORLD;
-  //
-  //  /* Initialize MPI */
-  //  rc = MPI_Init(&argc, &argv);
-  //
-  //  rc = MPI_Comm_rank(mpi_comm_global, &mpi_rank_global);
-  //  rc = MPI_Comm_size(mpi_comm_global, &mpi_numPEs_global);
-  //
-  //  /* Get info about MPI version, Communicator size, rank, processor name */
-  //  rc = MPI_Get_version(&mpiversion, &mpisubversion);
-  //  if (!mpi_rank_global)
-  //    printf("MPI version: %d.%d\n", mpiversion, mpisubversion);
-  //
-  //  rc = MPI_Get_processor_name(processor_name, &procnamelen);
-  //  if (rc != MPI_SUCCESS)
-  //    rc = MPI_Abort(MPI_COMM_WORLD, 1);
-  //  printf("Hello from %s rank %d, member of a communicator with %d PEs\n",
-  //         processor_name, mpi_rank_global, mpi_numPEs_global);
-  //
-  //  /* Finalize MPI */
-  //  MPI_Finalize();
+  MPI_Init_thread(&argc, &argv, required, &provided);
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
 
-  MPI_Init(&argc, &argv);
-  int myrank, nproc;
-  MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-  MPI_Comm_size(MPI_COMM_WORLD, &nproc);
-
-  std::ifstream file("example.json");
-  json data = json::parse(file);
-
-  auto p2 = data.template get<graph::reversibleVecGraph>();
-
-  uint32_t num_fin_vertex = 0;
-
-#pragma omp parallel
+#pragma omp parallel private(omp_rank)
   {
-#pragma omp for
-    for (int i = myrank; i < p2.number_of_vertices; i += nproc) {
-      int x = p2.dijkstra(i)[1270];
-
-#pragma omp critical
-      {
-        num_fin_vertex += 1;
-
-        if (num_fin_vertex % 1000 == 0) {
-
-          float progress_percent =
-              (float)num_fin_vertex / (float)p2.number_of_vertices * 100.0;
-
-          printf("%f%%\n", progress_percent);
-        }
-      }
-    }
+    omp_rank = omp_get_thread_num();
+    printf("I'm thread %d in process %d\n", omp_rank, my_id);
   }
+
+  if (my_id == 0) {
+    printf("\nProvided thread support level: %d\n", provided);
+    printf("  %d - MPI_THREAD_SINGLE\n", MPI_THREAD_SINGLE);
+    printf("  %d - MPI_THREAD_FUNNELED\n", MPI_THREAD_FUNNELED);
+    printf("  %d - MPI_THREAD_SERIALIZED\n", MPI_THREAD_SERIALIZED);
+    printf("  %d - MPI_THREAD_MULTIPLE\n", MPI_THREAD_MULTIPLE);
+  }
+  MPI_Finalize();
   return 0;
 }
