e18f303d26aced166671c598cd5a570f41710078
Christian Staib <staib.christian@hotmail.de>
2024-06-20 23:47:36 +0200

    added stuff

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,7 @@ int main(int argc, char **argv) {
   MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
   MPI_Comm_size(MPI_COMM_WORLD, &nproc);
 
+  printf("loading file\n");
   std::ifstream file("example.json");
   json data = json::parse(file);
 
