a5c19db29be08553d34e63f0f054498620a268bc
Christian Staib <staib.christian@hotmail.de>
2024-06-21 00:33:06 +0200

    added file

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,15 @@
-// Compile with: mpicc -fopenmp hello_hybrid.c -o hello_hybrid
-
+#include <cstdio>
 #include <mpi.h>
 #include <omp.h>
-#include <stdio.h>
-
 int main(int argc, char *argv[]) {
-  int num_procs, rank, namelen;
-  char processor_name[MPI_MAX_PROCESSOR_NAME];
-  int thread_nr = 0;
-
-  MPI_Init(&argc, &argv);
-  MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Get_processor_name(processor_name, &namelen);
-
-  if (rank == 0) {
-    printf("There are %d processes in total\n", num_procs);
-  }
-
-#pragma omp parallel default(shared) private(thread_nr)
+  int my_id, omp_rank;
+  int provided, required = MPI_THREAD_FUNNELED;
+  MPI_Init_thread(&argc, &argv, required, &provided);
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
+#pragma omp parallel private(omp_rank)
   {
-#pragma omp single
-    {
-      int num_threads = omp_get_num_threads();
-      printf("There are %d threads of process %d on %s\n", num_threads, rank,
-             processor_name);
-    }
-    thread_nr = omp_get_thread_num();
-    printf("Hello World from thread %d from process %d on node %s\n", thread_nr,
-           rank, processor_name);
+    omp_rank = omp_get_thread_num();
+    printf("I'm thread %d in process %d\n", omp_rank, my_id);
   }
-
   MPI_Finalize();
 }
