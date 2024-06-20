c5d17550dea5f9c80b286f27b045689d4031ba8e
Christian Staib <staib.christian@hotmail.de>
2024-06-20 23:24:00 +0200

    did stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,48 @@
+#include "graph.hpp"
+
+#include <chrono>
+#include <cstdint>
+#include <cstdio>
+#include <fstream>
+#include <iostream>
 #include <mpi.h>
-#include <omp.h>
-#include <stdio.h>
+#include <nlohmann/json.hpp>
 
-int main(int argc, char *argv[]) {
-  int num_procs, rank, namelen;
-  char processor_name[MPI_MAX_PROCESSOR_NAME];
-  int thread_nr = 0;
+using json = nlohmann::json;
 
+int main(int argc, char **argv) {
   MPI_Init(&argc, &argv);
-  MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Get_processor_name(processor_name, &namelen);
+  int myrank, nproc;
+  MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+  MPI_Comm_size(MPI_COMM_WORLD, &nproc);
 
-  if (rank == 0) {
-    printf("There are %d processes in total\n", num_procs);
+  std::ifstream file("example.json");
+  json data = json::parse(file);
+
+  auto p2 = data.template get<graph::reversibleVecGraph>();
+
+  uint32_t num_fin_vertex = 0;
+
+  // #pragma omp parallel
+  //   {
+  // #pragma omp for
+  for (int i = myrank; i < p2.number_of_vertices; i += nproc) {
+    printf("am at %d\n", i);
+    int x = p2.dijkstra(i)[1270];
+
+    // #pragma omp critical
+    //       {
+    //         num_fin_vertex += 1;
+    //
+    if (i % 1000 == 0) {
+
+      float progress_percent =
+          (float)num_fin_vertex / (float)p2.number_of_vertices * 100.0;
+
+      printf("%f%%\n", progress_percent);
+    }
+    //       }
+    //    }
   }
-
-#pragma omp parallel default(shared) private(thread_nr)
-  {
-    // #pragma omp single
-    //     {
-    //       int num_threads = omp_get_num_threads();
-    //       printf("There are %d threads of process %d on %s\n", num_threads,
-    //       rank,
-    //              processor_name);
-    //     }
-
-    printf("x\n");
-
-    // thread_nr = omp_get_thread_num();
-    // printf("Hello World from thread %d from process %d on node %s\n",
-    // thread_nr,
-    //        rank, processor_name);
-  }
-
-  MPI_Finalize();
+  return 0;
 }
