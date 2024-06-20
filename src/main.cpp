10962c38c679b5514e41976ad5135d47690b9320
Christian Staib <staib.christian@hotmail.de>
2024-06-21 00:51:40 +0200

    added stuff

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,8 @@ int main(int argc, char *argv[]) {
     printf("There are %d processes in total\n", num_procs);
   }
 
+  printf("rank:%d num_procs:%d\n", rank, num_procs);
+
   printf("loading file\n");
   std::ifstream file("example.json");
   nlohmann::json data = nlohmann::json::parse(file);
@@ -27,25 +29,25 @@ int main(int argc, char *argv[]) {
   auto p2 = data.template get<graph::reversibleVecGraph>();
   printf("there are %d vertices\n", p2.number_of_vertices);
 
-#pragma omp parallel
-  {
-#pragma omp for
-    for (int vertex = rank; vertex < p2.number_of_vertices;
-         vertex += num_procs) {
-
-      // if (vertex % 1000 == 0) {
-      printf("%f\n", (float)vertex / (float)p2.number_of_vertices * 100.0);
-      // }
-
-      int thread_nr = omp_get_thread_num();
-      int num_threads = omp_get_num_threads();
-
-      // printf("%d t:%d, p:%d, n:%s, num_threads:%d\n", vertex, thread_nr,
-      // rank,
-      //        processor_name, num_threads);
-      int32_t weight = p2.dijkstra(vertex)[123];
-    }
-  }
+  // #pragma omp parallel
+  //   {
+  // #pragma omp for
+  //     for (int vertex = rank; vertex < p2.number_of_vertices;
+  //          vertex += num_procs) {
+  //
+  //       // if (vertex % 1000 == 0) {
+  //       printf("%f\n", (float)vertex / (float)p2.number_of_vertices * 100.0);
+  //       // }
+  //
+  //       int thread_nr = omp_get_thread_num();
+  //       int num_threads = omp_get_num_threads();
+  //
+  //       // printf("%d t:%d, p:%d, n:%s, num_threads:%d\n", vertex, thread_nr,
+  //       // rank,
+  //       //        processor_name, num_threads);
+  //       int32_t weight = p2.dijkstra(vertex)[123];
+  //     }
+  //   }
 
   MPI_Finalize();
 }
