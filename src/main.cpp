933915e357e93c4d26036c76e3c5986b8d1b7ace
Christian Staib <staib.christian@hotmail.de>
2024-06-27 19:37:39 +0200

    did stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,9 @@
-#include "graph.hpp"
 #include <cstdint>
 #include <cstdio>
-#include <format>
-#include <fstream>
 #include <mpi.h>
-#include <nlohmann/json.hpp>
 #include <omp.h>
 #include <random>
-#include <vector>
-#include <zpp_bits.h>
+#include <sched.h>
 
 int main(int argc, char *argv[]) {
   int num_procs, rank, namelen;
@@ -18,44 +13,6 @@ int main(int argc, char *argv[]) {
   MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Get_processor_name(processor_name, &namelen);
-
-  //
-  // setup graph over all nodes
-  //
-
-  std::vector<std::byte> data;
-
-  if (rank == 0) {
-    // read the graph
-    graph::reversibleVecGraph graph;
-    std::ifstream graph_file("example.json");
-    nlohmann::json graph_data = nlohmann::json::parse(graph_file);
-    graph = graph_data.template get<graph::reversibleVecGraph>();
-
-    // serialize the graph
-    auto out_sender = zpp::bits::out(data);
-    (void)out_sender(graph);
-  }
-
-  // broadcast size of graph
-  int data_size = data.size();
-  MPI_Bcast(&data_size, 1, MPI_INT64_T, 0, MPI_COMM_WORLD);
-
-  // broadcast graph
-  data.reserve(data_size);
-  MPI_Bcast(data.data(), data_size, MPI_BYTE, 0, MPI_COMM_WORLD);
-
-  // deserialze graph
-  auto out = zpp::bits::in(data);
-  graph::reversibleVecGraph graph;
-  (void)out(graph);
-
-  //
-  // calculates random paths
-  //
-
-  // vector to save all calculated paths
-  std::vector<graph::path> paths = std::vector<graph::path>();
 
   printf("starting path calculations\n");
 #pragma omp parallel
@@ -69,31 +26,25 @@ int main(int argc, char *argv[]) {
     std::mt19937 gen(rd());
 
     // Define the distribution range. Max is inclusive therfore -1
-    std::uniform_int_distribution<> dis(0, graph.number_of_vertices - 1);
+    std::uniform_int_distribution<> dis(0, 99999);
 
     printf("hi from thread %d of %d on %d \n", omp_get_thread_num(),
            omp_get_num_threads(), sched_getcpu());
 
 #pragma omp for
-    for (int vertex = rank; vertex < graph.number_of_vertices;
-         vertex += num_procs) {
+    for (int vertex = rank; vertex < 1000000; vertex += num_procs) {
       for (int reps = 0; reps < 1; ++reps) {
         uint32_t source = dis(gen);
         uint32_t target = dis(gen);
-        auto path = graph.dijkstra(source, target);
 
-        if (path) {
-#pragma omp critical
-          paths.push_back(*path);
+        // dummy work to keep threads running
+        for (; target > 0; --target) {
         }
       }
     }
   }
 
   printf("finish %d \n", omp_get_thread_num());
-  nlohmann::json j = paths;
-  std::ofstream out_file(std::format("paths_%s.json", processor_name));
-  out_file << j;
 
   MPI_Finalize();
 }
