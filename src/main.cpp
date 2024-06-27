fc51070dfb7b01ed69ee6655fadf416c16eb12e2
Christian Staib <staib.christian@hotmail.de>
2024-06-27 18:52:48 +0200

    dif stuff

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <mpi.h>
 #include <nlohmann/json.hpp>
+#include <omp.h>
 #include <random>
 #include <vector>
 #include <zpp_bits.h>
@@ -19,83 +20,77 @@ int main(int argc, char *argv[]) {
   MPI_Get_processor_name(processor_name, &namelen);
 
   //
-  // SENDER
+  // setup graph over all nodes
   //
+
+  std::vector<std::byte> data;
 
   if (rank == 0) {
-    std::vector<std::byte> data;
+    // read the graph
+    graph::reversibleVecGraph graph;
+    std::ifstream graph_file("example.json");
+    nlohmann::json graph_data = nlohmann::json::parse(graph_file);
+    graph = graph_data.template get<graph::reversibleVecGraph>();
 
-    // initialize a path
-    graph::path paths_sender;
-    paths_sender.vertices = {1, 2, 3, 4};
-    paths_sender.weight = 10;
-
-    std::vector<graph::path> v1 = {paths_sender};
+    // serialize the graph
     auto out_sender = zpp::bits::out(data);
-    (void)out_sender(v1);
-
-    printf("sender data size is %lu\n", data.size());
-    MPI_Send(data.data(), data.size(), MPI_BYTE, 1, 0, MPI_COMM_WORLD);
+    (void)out_sender(graph);
   }
 
-  //
-  // RECEIVER
-  //
+  // broadcast size of graph
+  int data_size = data.size();
+  MPI_Bcast(&data_size, 1, MPI_INT64_T, 0, MPI_COMM_WORLD);
 
-  if (rank == 1) {
-    MPI_Status status;
-    int data_size;
+  // broadcast graph
+  data.reserve(data_size);
+  MPI_Bcast(data.data(), data_size, MPI_BYTE, 0, MPI_COMM_WORLD);
 
-    // Probe for the incoming message to get the size
-    MPI_Probe(0, 0, MPI_COMM_WORLD, &status);
-    MPI_Get_count(&status, MPI_BYTE, &data_size);
-    printf("receiver data size is %d\n", data_size);
-
-    std::vector<std::byte> data(data_size);
-
-    MPI_Recv(data.data(), data_size, MPI_BYTE, 0, 0, MPI_COMM_WORLD, &status);
-
-    std::vector<graph::path> paths_receiver;
-    auto out = zpp::bits::in(data);
-    (void)out(paths_receiver);
-
-    printf("weight is %d\n", paths_receiver[0].weight);
-    printf("length of vector<bytes> is %lu\n", data.size());
-  }
+  // deserialze graph
+  auto out = zpp::bits::in(data);
+  graph::reversibleVecGraph graph;
+  (void)out(graph);
 
   //
+  // calculates random paths
   //
-  //
-
-  // read the graph
-  std::ifstream graph_file("example.json");
-  nlohmann::json graph_data = nlohmann::json::parse(graph_file);
-  auto graph = graph_data.template get<graph::reversibleVecGraph>();
 
   // vector to save all calculated paths
   std::vector<graph::path> paths = std::vector<graph::path>();
 
-  // Create a random device and use it to seed the generator
-  std::random_device rd;
-  std::mt19937 gen(rd());
+  printf("starting path calculations\n");
+#pragma omp parallel
+  {
+    //
+    // setup thread local random number generator
+    //
 
-  // Define the distribution range. Max is inclusive therfore -1
-  std::uniform_int_distribution<> dis(0, graph.number_of_vertices - 1);
+    // Create a random device and use it to seed the generator
+    std::random_device rd;
+    std::mt19937 gen(rd());
 
-  for (int vertex = rank; vertex < graph.number_of_vertices;
-       vertex += num_procs) {
+    // Define the distribution range. Max is inclusive therfore -1
+    std::uniform_int_distribution<> dis(0, graph.number_of_vertices - 1);
 
-    for (int reps = 0; reps < 10; ++reps) {
-      uint32_t source = dis(gen);
-      uint32_t target = dis(gen);
-      auto path = graph.dijkstra(source, target);
+    printf("hi from thread %d of %d on %d \n", omp_get_thread_num(),
+           omp_get_num_threads(), sched_getcpu());
 
-      if (path) {
-        paths.push_back(*path);
+#pragma omp for
+    for (int vertex = rank; vertex < graph.number_of_vertices;
+         vertex += num_procs) {
+      for (int reps = 0; reps < 1; ++reps) {
+        uint32_t source = dis(gen);
+        uint32_t target = dis(gen);
+        auto path = graph.dijkstra(source, target);
+
+        if (path) {
+#pragma omp critical
+          paths.push_back(*path);
+        }
       }
     }
   }
 
+  printf("finish %d \n", omp_get_thread_num());
   nlohmann::json j = paths;
   std::ofstream out_file(std::format("paths_%s.json", processor_name));
   out_file << j;
