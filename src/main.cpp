5d5740e609ef633dafee393dad459e21da336ec7
Christian Staib <staib.christian@hotmail.de>
2024-11-15 13:37:30 +0100

    did stuff

--- src/main.cpp
+++ src/main.cpp
@@ -141,11 +141,10 @@ int main(int argc, char **argv) {
   for (int iteration = 0; iteration < num_iterations; ++iteration) {
     // x_{i + 1} = x_i + v_i * dt + 0.5 * a_i dt^2
     // v_{i + 1} = v_i + 0.5 (a_i + a_{i + 1}) * dt
-    // if (world_rank == 0) {
-    //   loging(bodies, global_body_system, vis_step_size_hours, step_size_days, myfile, iteration, num_iterations,
-    //   &begin,
-    //          bar.get());
-    // }
+    if (world_rank == 0) {
+      loging(bodies, global_body_system, vis_step_size_hours, step_size_days, myfile, iteration, num_iterations, &begin,
+             bar.get());
+    }
 
     naive_calculations::update_positions(local_body_system, step_size_days);
 
