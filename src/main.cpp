0f162e7ea34d67d3b135887b55da908c953dd380
Christian Staib <staib.christian@hotmail.de>
2024-11-15 13:29:15 +0100

    did stuff

--- src/main.cpp
+++ src/main.cpp
@@ -48,7 +48,7 @@ void loging(std::vector<space::CelestialBody> &bodies, space::BodySystem &body_s
     // double ke = naive_calculations::get_kinetic_energy(body_system.num_bodies, body_system.mass,
     // body_system.velocity); double pe =
     //     naive_calculations::get_potential_energy(body_system.num_bodies, body_system.mass, body_system.position);
-    csv_writer::write_data(myfile, body_system.position, bodies);
+    // csv_writer::write_data(myfile, body_system.position, bodies);
   }
 }
 
